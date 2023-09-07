d608146ec80ea51ea4c6f70ca22891b579749a4b
BFDavidGamboa <david.gamboa@blankfactor.com>
2023-09-07 18:59:59 -0500

    added myfirstneuron.py which calculates basic vectors operations for a single array, weights, and biases for same lenght vectors, also created lossfunction.py with the implementation of a Forward pass example of a neural network model

---
+++ src/utils/printneurons.py
@@ -0,0 +1,4 @@
+def print_inputs(print_input, print_weights, print_bias):
+    print("array of inputs", print_input)
+    print("array of weights", print_weights)
+    print("array of bias", print_bias)
